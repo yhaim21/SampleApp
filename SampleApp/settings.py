49e83272c15128416768c753829d8184f1d6cc47
yhaim21 <71280147+yhaim21@users.noreply.github.com>
2021-03-21 23:52:26 -0400

    changes last night

--- SampleApp/settings.py
+++ SampleApp/settings.py
@@ -124,8 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
 LOGIN_REDIRECT_URL = '/home'
 LOGOUT_REDIRECT_URL = '/home'
-
 django_heroku.settings(locals())
